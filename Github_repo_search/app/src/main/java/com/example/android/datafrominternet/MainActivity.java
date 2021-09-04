d967db1fa55ae57da48a20c3f3cd3bb1b548377b
Tanmay Daga <tanmay06daga@gmail.com>
2021-09-05 00:12:53 +0530

    Sunshine till showing data in textview completed

--- Github_repo_search/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Github_repo_search/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -2,24 +2,37 @@ package com.example.android.datafrominternet;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.datafrominternet.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    EditText mSearchBoxEditText;
+    private EditText mSearchBoxEditText;
 
-    TextView mUrlDisplayTextView;
+    private TextView mUrlDisplayTextView;
 
-    TextView mSearchResultsTextView;
+    private TextView mSearchResultsTextView;
 
+    private TextView mErrorMessageDisplayTextview;
+
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +43,76 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = (EditText) findViewById(R.id.search_box);
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+        mErrorMessageDisplayTextview = (TextView) findViewById(R.id.tvShowError);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.PBLoadingIndicator);
 
 
     }
 
-    @Override
+    private void showJson(){
+        mErrorMessageDisplayTextview.setVisibility(View.INVISIBLE);
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+
+    }
+    private void showErrorMessage(){
+        mErrorMessageDisplayTextview.setVisibility(View.VISIBLE);
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void makeGithubSearchQuery(){
+        String githubSearchQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubSearchQuery);
+        if(TextUtils.isEmpty(githubSearchUrl.toString())){
+            mUrlDisplayTextView.setText("No url can be formed");
+        }
+        else {
+            mUrlDisplayTextView.setText(githubSearchUrl.toString());
+            new GithubQueryClass().execute(githubSearchUrl);
+        }
+
+
+
+
+
+    }
+
+    public class GithubQueryClass extends AsyncTask<URL, Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String githubSearchResult = null;
+            try {
+                githubSearchResult = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResult;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (!TextUtils.isEmpty(s)){
+
+                mSearchResultsTextView.setText(s);
+                showJson();
+            }
+            else {
+                showErrorMessage();
+            }
+        }
+    }
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
@@ -44,9 +122,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
        int itemThatWasClickedId = item.getItemId();
        if (itemThatWasClickedId == R.id.action_search){
-           Context context = MainActivity.this;
-           String textToShow = "Search clicked";
-           Toast.makeText(context,textToShow,Toast.LENGTH_LONG).show();
+           makeGithubSearchQuery();
+
            return true;
 
        }
--- Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,27 +1,121 @@
 package com.example.android.sunshine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+    private TextView errorMessageTextView;
+    private ProgressBar mLoadingIndicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forecast);
 
         mWeatherTextView = (TextView) findViewById(R.id.weather_data);
-        String[] dummyDatas={
-                "today, May 17 - Cleat - 17*C / 15*C",
-                ""
-        };
-        for (String dummyData:
+        errorMessageTextView = (TextView) findViewById(R.id.error_message_display);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.progress_bar);
+        loadWeatherData();
 
-            dummyDatas ) {
-            mWeatherTextView.append(dummyData);
+    }
+
+    private void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new fetchWeatherTask().execute(location);
+    }
+
+    private void showWeatherDataView(){
+        errorMessageTextView.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+    private void showErrorMessage(){
+        errorMessageTextView.setVisibility(View.VISIBLE);
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+    }
+
+    public class fetchWeatherTask extends AsyncTask<String, Void, String[]>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
         }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            /*if zip code empty or not*/
+            if (params.length == 0){
+                return null;
+            }
+            else{
+                String location = params[0];
+                URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+                try {
+                    String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+                    String[] simpleJsonWeatherData = OpenWeatherJsonUtils.
+                            getSimpleWeatherStringsFromJson(MainActivity.this,jsonWeatherResponse);
+                    return simpleJsonWeatherData;
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    return null;
+                }
+
+
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (weatherData != null){
+                showWeatherDataView();
+                for (String weatherString: weatherData) {
+                    mWeatherTextView.append((weatherString)+"\n\n");
+                }
+
+            }
+            else {
+                showErrorMessage();
+            }
+
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh){
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
