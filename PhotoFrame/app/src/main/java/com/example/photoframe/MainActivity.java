7ca686bf24c5daa9ca869fb65cd9c08457a751f6
Tanmay Daga <tanmay06daga@gmail.com>
2021-10-03 20:51:38 +0530

    Photoframe an practice app

--- PhotoFrame/app/src/main/java/com/example/photoframe/MainActivity.java
+++ PhotoFrame/app/src/main/java/com/example/photoframe/MainActivity.java
@@ -2,46 +2,45 @@ package com.example.photoframe;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements PhotoAdapter.onClickHandler{
 
     private RecyclerView mRecyclerView;
     private PhotoAdapter mPhotoAdapter;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.text_view);
         mRecyclerView = findViewById(R.id.recyclerView);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,GridLayoutManager.DEFAULT_SPAN_COUNT);
-        mRecyclerView.setLayoutManager(gridLayoutManager);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
 
-        mPhotoAdapter = new PhotoAdapter();
+        mPhotoAdapter = new PhotoAdapter(this);
         mRecyclerView.setAdapter(mPhotoAdapter);
+
+
 
     }
 
-    //TODO
-    public class fetchPhotos extends AsyncTask<Void,Void, Bitmap[]>{
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Bitmap[] doInBackground(Void... voids) {
-            return new Bitmap[0];
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap[] bitmaps) {
-            super.onPostExecute(bitmaps);
-        }
+    @Override
+    public void onClick(String text) {
+        textView.setText(text);
     }
 }
--- PhotoFrame/app/src/main/java/com/example/photoframe/PhotoAdapter.java
+++ PhotoFrame/app/src/main/java/com/example/photoframe/PhotoAdapter.java
@@ -2,6 +2,9 @@ package com.example.photoframe;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,20 +12,35 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 //TODO itemclicklistner
 public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoAdapterViewHolder>{
     Bitmap[] data;
+    private Context context;
+    public onClickHandler AdapterclickHandler;
+    public interface onClickHandler{
+        public void onClick(String text);
+    }
+
+    public PhotoAdapter(onClickHandler onClickHadler){AdapterclickHandler = onClickHadler;}
     @NonNull
     @Override
     public PhotoAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
+
         return new PhotoAdapterViewHolder(LayoutInflater.from(context).inflate(R.layout.grid_item,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull PhotoAdapterViewHolder holder, int position) {
-        Bitmap image = data[position];
-        holder.imageView.setImageBitmap(image);
+        new fetchPhotos().execute(holder.imageView);
+
     }
 
     @Override
@@ -30,15 +48,86 @@ public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoAdapter
         return 12;//Only for trial
     }
 
-    public class PhotoAdapterViewHolder extends RecyclerView.ViewHolder {
+    public class PhotoAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public final ImageView imageView;
         public PhotoAdapterViewHolder(@NonNull View itemView) {
 
             super(itemView);
+            itemView.setOnClickListener(this::onClick);
             imageView = (ImageView) itemView.findViewById(R.id.image_view);
+
 
 
         }
 
+        @Override
+        public void onClick(View view) {
+            AdapterclickHandler.onClick(String.valueOf(getAdapterPosition()));
+        }
     }
-}
+    public  void setImages(Bitmap[] images){
+        data = images;
+        notifyDataSetChanged();
+    }
+
+    public class fetchPhotos extends AsyncTask<ImageView, Void, Bitmap> {
+        public ImageView[] imgview;
+        @Override
+        protected Bitmap doInBackground(ImageView... imageViews) {
+            imgview = imageViews;
+            return getImage("https://source.unsplash.com/random");
+
+
+        }
+
+        private Bitmap getImage(String imageUrl)
+        {
+            Bitmap image = null;
+            int inSampleSize = 0;
+
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            options.inJustDecodeBounds = true;
+
+            options.inSampleSize = inSampleSize;
+
+            try
+            {
+                URL url = new URL(imageUrl);
+
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                InputStream stream = connection.getInputStream();
+
+                image = BitmapFactory.decodeStream(stream, null, options);
+
+
+
+                    options.inJustDecodeBounds = false;
+
+                    connection = (HttpURLConnection)url.openConnection();
+
+                    stream = connection.getInputStream();
+
+                    image = BitmapFactory.decodeStream(stream, null, options);
+
+                    return image;
+
+            }
+
+            catch(Exception e)
+            {
+                Log.e("getImage", e.toString());
+            }
+
+            return image;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imgview[0].setImageBitmap(bitmap);
+
+        }
+    }}
