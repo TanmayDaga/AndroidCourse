3d34479e376a271ee99227d267bcc6b2802e9ba7
Tanmay Daga <tanmay06daga@gmail.com>
2021-12-19 23:09:18 +0530

    Implementation of database left

--- Create-Entry-master/app/src/main/java/com/example/android/todolist/database/TaskEntry.java
+++ Create-Entry-master/app/src/main/java/com/example/android/todolist/database/TaskEntry.java
@@ -1,23 +1,33 @@
 package com.example.android.todolist.database;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import java.util.Date;
 
-// TODO (2) Annotate the class with Entity. Use "task" for the table name
+@Entity(tableName = "task")
 public class TaskEntry {
 
-    // TODO (3) Annotate the id as PrimaryKey. Set autoGenerate to true.
+
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private String description;
     private int priority;
+
+    @ColumnInfo(name = "updated_at")
     private Date updatedAt;
 
-    // TODO (4) Use the Ignore annotation so Room knows that it has to use the other constructor instead
+//    When updation
+    @Ignore
     public TaskEntry(String description, int priority, Date updatedAt) {
         this.description = description;
         this.priority = priority;
         this.updatedAt = updatedAt;
     }
 
+//    when creation
     public TaskEntry(int id, String description, int priority, Date updatedAt) {
         this.id = id;
         this.description = description;
